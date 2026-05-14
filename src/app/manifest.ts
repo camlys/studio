b7893d518ffecf8265a8b9f5963da108924a0d1f
Camly <camlysales@gmail.com>
2026-05-14 21:16:43 +0000

    Now icon showing too big , reduce little bit

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,19 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/camly.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any',
       },
       {
         src: '/camly.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
-      },
-      {
-        src: '/camly.png',
-        sizes: '1024x1024',
-        type: 'image/png',
-        purpose: 'any maskable',
       },
     ],
   };

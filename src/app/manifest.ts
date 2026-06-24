c0e52bb3ccaddff87c2e2984589207cc8dcf29b2
Camly <camlysales@gmail.com>
2026-06-24 16:46:33 +0000

    On installing app, show then camly.png logo on system. Ensure logo icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -18,6 +18,12 @@ export default function manifest(): MetadataRoute.Manifest {
       },
       {
         src: '/camly.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/camly.png',
         sizes: '384x384',
         type: 'image/png',
         purpose: 'any',
@@ -27,6 +33,12 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
+      },
+      {
+        src: '/camly.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
